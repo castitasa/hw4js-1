5b8cb06148a540e28b9480142fb7e02c7e6b6ada
castitasa <aibike0806@gmail.com>
2023-09-12 17:26:45 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let arr = [
+    { id: 1, name: "Вася" },
+    { id: 2, name: "Петя" },
+    { id: 3, name: "Маша" }
+];
+
+
+
+console.log(arr.find((item,) => {
+    return (item.id === 2)
+}));
